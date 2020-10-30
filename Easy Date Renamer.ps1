a41afcf09fa7c883a4484dda7f37ea781f59ed7f
IchibanDeath <ichibandeath@gmail.com>
2020-10-30 13:37:12 -0700

    update 30-1

--- Easy Date Renamer.ps1
+++ Easy Date Renamer.ps1
@@ -1,22 +1,23 @@
-﻿
-Start-Transcript
+﻿Start-Transcript -Path $HOME -NoClobber
 
-#Import Files to HD
-#Set SD location as 
+#Set SD location as $Import 
 #Set Destination as $Path
 
+#Import Files to HD
 $import = 'H:\Upgrade'
 $Path = 'F:\Test'
 $NewPath = 'F:\Testing'
 
+#Copy Items from SDcard/$Import
+
 Get-ChildItem -Path $import | Copy-Item -Destination $Path
 
+#Rename Files adding Dashes between MM DD YYYY and HHMMSS 
+#Still working on how to trim either right after or in the same operation
 
-Get-ChildItem -Path $Path *.mp3 | Rename-Item -NewName {$_.Name -Replace ('^\n*(\d{2})(\d{2})(\d{4})(\d{4})','TC $1-$2-$3-$4-')
-  | Rename-Item -NewName -WhatIf ($_.BaseName.Trim(0,12))
-
-}
-
+Get-ChildItem -Path $Path *.mp3 | Rename-Item -NewName {$_.Name -Replace ('^\n*(\d{2})(\d{2})(\d{4})(\d{4})','TC $1-$2-$3-$4-')}
+#Move files into $NewPath\Year\Month created
+#Running in to problems with moving the files after they have been renamed - Likely due to 
   Get-ChildItem -File -Path $Path |
     ForEach-Object {
 
@@ -27,8 +28,9 @@ Get-ChildItem -Path $Path *.mp3 | Rename-Item -NewName {$_.Name -Replace ('^\n*(
         $filename = $_.Name
 
         if (-not (Test-Path -Path $ArchDir)) { New-Item -ItemType "directory" -Path $ArchDir | Out-Null }
-        Move-Item -Path $filename -Destination $ArchDir -WhatIf
+        Move-Item -Path $Path -Destination $ArchDir -WhatIf
         }
 
 
+#Would like to add a list of files renamed and where they moved to instead of just Enter to exit
 Read-Host -Prompt "Press Enter to exit"
