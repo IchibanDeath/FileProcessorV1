72f717bf70d9adfac06156d88de8d8455ae5080d
IchibanDeath <ichibandeath@gmail.com>
2020-10-30 15:56:52 -0700

    30-4

--- Easy Date Renamer.ps1
+++ Easy Date Renamer.ps1
@@ -1,27 +1,39 @@
-﻿Start-Transcript
+﻿#Set SD location as $Import 
+#Set Temp Destination as $Path
+#Set Final Destination as $NewPath
 
-#Set SD location as $Import 
-#Set Destination as $Path
-
-#Import Files to HD
+#Import Files to HD for renaming while maintaining original copy
 
 $import = 'H:\Upgrade'
 $Path = 'F:\Test'
 $NewPath = 'F:\Testing'
 
-#Copy Items from SDcard/$Import
+#Test if $Path Exists, if not Create directory for Files to be transferred to
 
-Get-ChildItem -Path $import | Copy-Item -Destination $Path
+if(!(Test-Path -path $Path))  
+{  
+ New-Item -ItemType directory -Path $Path
+ Write-Host "Folder path has been created successfully at: " $Path    
+ }
+else 
+{ 
+Write-Host "The given folder path $Path already exists"; 
+}
+
+#Copy Items from SDcard/$import location
+
+Get-ChildItem -Path $import -Filter * '*.mp3' | Copy-Item -Destination $Path
 
 #Rename Files adding Dashes between MM DD YYYY and HHMMSS 
-#Still working on how to trim either right after or in the same operation
+#Still working on how to trim either right after or in the same operation to delete everything after YYYY
+#Will need to handle duplicate files from the same date (Would like a, b, c- instead of 1, 2, 3-)
 
-Get-ChildItem -Path $Path *.mp3 | Rename-Item -NewName {$_.Name -Replace ('^\n*(\d{2})(\d{2})(\d{4})(\d{4})','TC $1-$2-$3-$4-')}
+Get-ChildItem -Path $Path -Filter '*.mp3' | Rename-Item -NewName {$_.Name -Replace ('^\n*(\d{2})(\d{2})(\d{4})(\d{4})','TC $1-$2-$3-$4-')}
 
 #Move files into $NewPath\Year\Month created
 #Running in to problems with moving the files after they have been renamed - Likely due to 
 
-  Get-ChildItem -File -Path $Path |
+  Get-ChildItem -File -Path $Path -Filter '*mp3' |
     ForEach-Object {
 
         $Year = $_.LastWriteTime.Year
@@ -31,7 +43,7 @@ Get-ChildItem -Path $Path *.mp3 | Rename-Item -NewName {$_.Name -Replace ('^\n*(
         $filename = $_.Name
 
         if (-not (Test-Path -Path $ArchDir)) { New-Item -ItemType "directory" -Path $ArchDir | Out-Null }
-        Move-Item -Path $Path -Destination $ArchDir -WhatIf
+        Move-Item -Path $Path -Destination $ArchDir
         }
 
 
