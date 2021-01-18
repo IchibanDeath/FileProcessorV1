0f4f92be961958c46ee4146c06d83fbcdb8db1c4
IchibanDeath <ichibandeath@gmail.com>
2021-01-18 13:15:44 -0800

    1-18 Update Missing Trim and alpha enumeration for same date files

--- Date Renamer Practice Changes.ps1
+++ Date Renamer Practice Changes.ps1
@@ -1,70 +1,56 @@
 #Set SD location as $Import 
-#Set Temp Destination as $Path
-#Set Final Destination as $NewPath
-
+#Set Temp Destination as $Source
+#Set Final Destination as $Destination
 #Import Files to HD for renaming while maintaining original copy
+Clear-Host
+
 $date = Get-Date -Format 'dddd MM-dd-yyyy'
 Start-Transcript -Path "F:\$date.txt" -NoClobber -Append
 
+$Import = "C:\Users\IchibanDeath\Desktop\New Directory"
+$Source = "F:\Test\"
+$Destination = "F:\Testing\"
 
-$Import = 'C:\Users\IchibanDeath\Desktop\New Directory'
-$Path = 'F:\Test'
-$NewPath = 'F:\Testing'
-
-#Test if $Path Exists, if not Create directory for Files to be transferred to
-if(!(Test-Path -path $Path))  {  
- New-Item -ItemType directory -Path $Path
- Write-Host "Folder path has been created successfully at: " $Path    
+#Test if $Source Exists, if not Create directory for Files to be transferred to
+if(!(Test-Path -Path $Source))  {  
+ New-Item -ItemType directory -Path $Source
+ Write-Host "Folder path has been created successfully at: " $Source    
  }
 else { 
-Write-Host "The given folder path $Path already exists"; 
+Write-Host "The given folder path $Source already exists"; 
 }
-
+#Test if $Destination Exists, if not Create directory for Files final destination
+if(!(Test-Path -Path $Destination))  {  
+  New-Item -ItemType directory -Path $Destination
+  Write-Host "Folder path has been created successfully at: $Destination "}
+ else { 
+ Write-Host "The given folder path $Destination already exists"; 
+ }
+ 
 #Copy Items from $Import location
-Get-ChildItem -Path $Import -Filter '*.mp3' | Copy-Item -Destination $Path
-
+Get-ChildItem -Path $Import -Filter *.mp3 | Copy-Item -Destination $Source
 #Rename Files adding Dashes between MM DD YYYY and HHMMSS
+Get-ChildItem -Path $Source -Filter *.mp3 | Rename-Item -NewName {$_.Name -Replace ('^\n*(\d{2})(\d{2})(\d{4})(\d{6}).(?:...)(\d{3})\w','TC $1-$2-$3-$4')}
 
-Get-ChildItem -Path $Path -Filter '*.mp3' | Rename-Item -NewName {$_.Name -Replace ('^\n*(\d{2})(\d{2})(\d{4})(\d{6})','TC $1-$2-$3-')}
+#Move files into $Destination\Year\Month created - Need to add
 
-#Test if $NewPath exists and create if false
-if(!(Test-Path -path $NewPath))  {  
- New-Item -ItemType directory -Path $NewPath
- Write-Host "Folder path has been created successfully at: " $NewPath    
- }
-else { 
-Write-Host "The given folder path $NewPath already exists"; 
-}
-#Move files into $NewPath\Year\Month created
-#Running in to problems with moving the files after they have been renamed - Likely due to 
+$Files = Get-ChildItem -Path $Source -Filter *.mp3
 
-function fcopy ($SourceDir,$DestinationDir)
-{
-	Get-ChildItem $SourceDir -Recurse | Where-Object { $_.PSIsContainer -eq $false } | ForEach-Object ($_) {
-    $SourceFile = $_.FullName
-    $Year = $_.LastWriteTime.Year
-    $Month = $_.LastWriteTime.Month
-    $Monthname = (Get-Culture).DateTimeFormat.GetMonthName($Month)
-    $NewPath = 'F:\Testing'
-    $ArchDir = "$NewPath\$Year\$Monthname\"
-    #Array for iterate for same date, still trying to figure out how to get first file of a certain date to take the 'a'
-
+Foreach($File in $Files){
+	$FileBaseName = $File.BaseName.Trim(14)
+    $FileExtension = $File.Extension
     $myList = @("a","b","c","d","e","f","g","h","i","j")
-    $DestinationFile = $DestinationDir + $_
-  
-    if (Test-Path $DestinationFile) {
-			$i = 0
-			while (Test-Path $DestinationFile) { $i += 1
-				$DestinationFile = $DestinationDir + $_.basename + $myList[$i] + $_.extension
-			}
-    } else {
-			Copy-Item -Path $SourceFile -Destination $DestinationFile -Verbose -Force
-		}
-		Copy-Item -Path $SourceFile -Destination $DestinationFile -Verbose -Force
+	$i = 0
+	$Destination = "F:\Testing\"
+	$FileName = "{0}{1}{2}" -f $FileBaseName.Trim(15) + $myList[$i] + $FileExtension
+	While (Test-Path -Path $Source "$Destination\$Filename"){
+		$i++
+		$FileName = "{0}{1}{2}" -f $FileBaseName + $myList[$i] + $FileExtension
 	}
+	Remove-Variable i -ErrorAction SilentlyContinue
+	$NewFileDestination = "{0}{1}" -f $Destination + $FileName
+	Move-item $File.FullName -Destination $NewFileDestination -Verbose
 }
-fcopy -SourceDir "F:\Test\" -DestinationDir '$ArchDir'
 
-#Would like to add a list of files renamed and where they moved to instead of just Enter to exit
-
+Write-Host "Above Files Copied to $Destination"
 Read-Host -Prompt "Press Enter to exit"
--- Easy Date Renamer.ps1
+++ Easy Date Renamer.ps1
@@ -1,65 +1,50 @@
-﻿#Set SD location as $Import 
-#Set Temp Destination as $Path
-#Set Final Destination as $NewPath
-
-#Import Files to HD for renaming while maintaining original copy
+﻿#Start by Clearing Host and Saving Transcript
+Clear-Host
 $date = Get-Date -Format 'dddd MM-dd-yyyy'
-$i = 0  
-Start-Transcript -Path "F:\$date$i.txt"
+Start-Transcript -Path "F:\New$date.txt" -NoClobber -Append
+#Set locations - SD Card:Import
+$Import = "C:\Users\IchibanDeath\Desktop\New Directory"
+$Source = "F:\Test\"
+$Destination = "F:\Testing"
 
-$Import = 'H:\Upgrade'
-$Path = 'F:\Test'
-$NewPath = 'F:\Testing'
+#Test if $Source Exists, if not Create directory for Files to be transferred to
+if(!(Test-Path -Path $Source))  {  
+    New-Item -ItemType directory -Path $Source
+    Write-Host "Folder path has been created successfully at: " $Source    
+    }
+   else { 
+   Write-Host "The given folder path $Source already exists"; 
+   }
+   #Test if $Destination exists and create if false
+   if(!(Test-Path -Path $Destination))  {  
+     New-Item -ItemType directory -Path $Destination
+     Write-Host "Folder path has been created successfully at: $Destination "}
+    else { 
+    Write-Host "The given folder path $Destination already exists"; 
+    }
 
-#Test if $Path Exists, if not Create directory for Files to be transferred to
-
-if(!(Test-Path -path $Path))  
-{  
- New-Item -ItemType directory -Path $Path
- Write-Host "Folder path has been created successfully at: " $Path    
- }
-else 
-{ 
-Write-Host "The given folder path $Path already exists"; 
-}
-
-#Copy Items from $Import location
-
-Get-ChildItem -Path $Import -Filter '*.mp3' | Copy-Item -Destination $Path
-
-#Rename Files adding Dashes between MM DD YYYY and HHMMSS 
-#Still working on how to trim either right after or in the same operation to delete everything after YYYY
-#Will need to handle duplicate files from the same date (Would like a, b, c- instead of 1, 2, 3-)
-
-Get-ChildItem -Path $Path -Filter '*.mp3' | Rename-Item -NewName {$_.Name -Replace ('^\n*(\d{2})(\d{2})(\d{4})(\d{4})','TC $1-$2-$3-$4-')}
-
-#Test if $NewPath exists and create if false
-
-if(!(Test-Path -path $NewPath))  
-{  
- New-Item -ItemType directory -Path $NewPath
- Write-Host "Folder path has been created successfully at: " $NewPath    
- }
-else 
-{ 
-Write-Host "The given folder path $NewPath already exists"; 
-}
+   #Copy Items from $Import location
+   Get-ChildItem -Path $Import -Filter *.mp3 | Copy-Item -Destination $Source
+   #Rename Files adding Dashes between MM DD YYYY and HHMMSS
+   Get-ChildItem -Path $Source -Filter *.mp3 | Rename-Item -NewName {$_.Name -Replace ('^\n*(\d{2})(\d{2})(\d{4})(\d{6}).(?:...)(\d{3})\w','TC $1-$2-$3-$4')}
+   
+   #Move files into $Destination\Year\Month created - Need to add
 
 
 #Move files into $NewPath\Year\Month created
 #Running in to problems with moving the files after they have been renamed - Likely due to 
 
-  Get-ChildItem -File -Path $Path -Filter '*.mp3' |
-    ForEach-Object {
+Get-ChildItem -File -Path $Source -Filter '*.mp3' |
+ForEach-Object {
 
-        $Year = $_.LastWriteTime.Year
-        $Month = $_.LastWriteTime.Month
-        $Monthname = (Get-Culture).DateTimeFormat.GetMonthName($Month)
-        $ArchDir = "$NewPath\$Year\$Monthname\"
+    $Year = $_.LastWriteTime.Year
+    $Month = $_.LastWriteTime.Month
+    $Monthname = (Get-Culture).DateTimeFormat.GetMonthName($Month)
+    $ArchDir = "$Destination\$Year\$Monthname\"
 
-        if (-not (Test-Path -Path $ArchDir)) { New-Item -ItemType "directory" -Path $ArchDir | Out-Null }
-        Move-Item -Path $Path\*.mp3 -Destination $ArchDir -Verbose
-        }
+    if (-not (Test-Path -Path $ArchDir)) { New-Item -ItemType "directory" -Path $ArchDir | Out-Null }
+    Move-Item -Path $Source\*.mp3 -Destination $ArchDir -Verbose
+    }
 
 
 #Would like to add a list of files renamed and where they moved to instead of just Enter to exit
